e41c001e0bf97582c5cbb29367f16ab498d2784f
adrianvalerio <adrian_2004_valerio@hotmail.com>
2025-11-07 08:11:49 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ahorrau_aplicacionmovil/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.ahorrau_aplicacionmovil
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ahorrau_aplicacionmovil.ui.theme.AhorraUAplicacionMovilTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AhorraUAplicacionMovilTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AhorraUAplicacionMovilTheme {
+        Greeting("Android")
+    }
+}
