dfea586444cbc33630c2ffc9e3b8427c27f28128
adrianvalerio <adrian_2004_valerio@hotmail.com>
2025-11-07 14:54:55 -0500

    chore: upload theme for color app

--- app/src/main/java/com/example/ahorrau_aplicacionmovil/ui/theme/Color.kt
+++ app/src/main/java/com/example/ahorrau_aplicacionmovil/ui/theme/Color.kt
@@ -2,10 +2,24 @@ package com.example.ahorrau_aplicacionmovil.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Colores principales de AhorraU
+val YellowPrimary = Color(0xFFFFC107)
+val YellowLight = Color(0xFFFFD54F)
+val YellowDark = Color(0xFFFFA000)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PinkPrimary = Color(0xFFFF9AA2)
+val PinkLight = Color(0xFFFFB3BA)
+val PinkDark = Color(0xFFFF7D87)
+
+val GrayDark = Color(0xFF424242)
+val GrayMedium = Color(0xFF616161)
+val GrayLight = Color(0xFF9E9E9E)
+
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+
+val GreenSuccess = Color(0xFF4CAF50)
+val GreenLight = Color(0xFFE8F5E9)
+
+val RedError = Color(0xFFF44336)
+val RedLight = Color(0xFFFFEBEE)
